366db97497c6c69f8631b802aaf353c79de5e3df
Nigel <pratyushnbaxla79@gmail.com>
2023-02-15 01:07:55 +0000

    Add csrf trusted origins setting

--- InventoryManagement/settings.py
+++ InventoryManagement/settings.py
@@ -31,6 +31,8 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'x21179158inventorymanagement.azurewebsites.net', '127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://x21179158inventorymanagement.azurewebsites.net']
 
 # Application definition
 
